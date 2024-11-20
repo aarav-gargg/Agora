53c70b2e5d74f3a22bc22b004e5ebe7bd0266ef2
aarav-gargg <aaravgarg975@gmail.com>
2024-11-20 21:12:07 +0530

    "Added new blog controller functions and updated blog route to include new endpoints"

--- BackEnd/controllers/blog.controller.js
+++ BackEnd/controllers/blog.controller.js
@@ -1,26 +1,158 @@
 import Blogs from "../models/blogs.model.js";
 import User from "../models/users.model.js";
+import mongoose from "mongoose";
 
-export const createBlog = async (req,res) => {
+
+export const updateBlog = async (req, res) => {
     try {
-        const {title , content} = req.body;
+        const { title, content } = req.body;
+        const { blogId } = req.params;
 
-        const user = req.user
+        if (!mongoose.isValidObjectId(blogId)) {
+            return res.status(400).json({ message: "Invalid blog ID" });
+        }
 
-        const blog = new Blogs({title,content});
+        const updatedBlog = await Blogs.findByIdAndUpdate(blogId, { title, content }, { new: true });
+
+        if (!updatedBlog) {
+            return res.status(400).json({ message: "No such blog exists" });
+        }
+
+        return res.status(200).json({
+            message: "Blog updated successfully",
+            updatedBlog
+        });
+    } catch (error) {
+        return res.status(400).json({
+            message: error.message
+        });
+    }
+};
+
+export const getAllBlogs = async (req, res) => {
+    try {
+        const allBlogs = await Blogs.find();
+        return res.status(200).json({
+            message: "Blogs fetched successfully",
+            allBlogs
+        });
+    } catch (error) {
+        return res.status(400).json({
+            message: error.message
+        });
+    }
+};
+
+export const getUserBlogs = async (req, res) => {
+    try {
+        const user = req.user;
+        const userId = user.id;
+
+        const userBlogs = await User.findById(userId).populate({
+            path: 'blogs',
+            options: { sort: { createdAt: -1 } }
+        });
+
+        if (!userBlogs || userBlogs.blogs.length === 0) {
+            return res.status(404).json({
+                message: "No blogs found for this user"
+            });
+        }
+
+        return res.status(200).json({
+            message: "Blogs fetched successfully",
+            blogs: userBlogs.blogs
+        });
+    } catch (error) {
+        return res.status(400).json({
+            message: error.message
+        });
+    }
+};
+
+export const getBlogById = async (req, res) => {
+    try {
+        const { blogId } = req.params;
+
+        if (!mongoose.isValidObjectId(blogId)) {
+            return res.status(400).json({ message: "Invalid blog ID" });
+        }
+
+        const blog = await Blogs.findById(blogId);
+
+        if (!blog) {
+            return res.status(400).json({
+                message: "No such blog exists"
+            });
+        }
+
+        return res.status(200).json({
+            message: "Blog fetched successfully",
+            blog
+        });
+    } catch (error) {
+        return res.status(400).json({
+            message: error.message
+        });
+    }
+};
+
+export const deleteBlog = async (req, res) => {
+    try {
+        const { blogId } = req.params;
+
+        const user = req.user;
+        const userId = user.id;
+
+        if (!mongoose.isValidObjectId(blogId)) {
+            return res.status(400).json({ message: "Invalid blog ID" });
+        }
+
+        const foundedUser = await User.findById(userId);
+
+        if (foundedUser.role !== 'Admin') {
+            return res.status(403).json({
+                message: "You are not authorized to delete the post"
+            });
+        }
+
+        const deletedBlog = await Blogs.findByIdAndDelete(blogId);
+
+        if (!deletedBlog) {
+            return res.status(400).json({
+                message: "No such blog exists"
+            });
+        }
+
+        return res.status(200).json({
+            message: "Blog deleted successfully"
+        });
+    } catch (error) {
+        return res.status(400).json({
+            message: error.message
+        });
+    }
+};
+
+export const createBlog = async (req, res) => {
+    try {
+        const { title, content } = req.body;
+
+        const user = req.user;
+
+        const blog = new Blogs({ title, content });
         const savedBlog = await blog.save();
 
         user.blogs.push(savedBlog._id);
         await user.save();
 
         res.status(201).json({
-            message:"Blog created successfully",
-            blog:savedBlog
+            message: "Blog created successfully",
+            blog: savedBlog
         });
-
     } catch (error) {
         return res.status(400).json({
             message: error.message
-        })
+        });
     }
-}
+};
--- BackEnd/routes/blog.route.js
+++ BackEnd/routes/blog.route.js
@@ -1,9 +1,15 @@
 import express from 'express'
-import { createBlog } from '../controllers/blog.controller.js'
+import { createBlog , getAllBlogs , getBlogById , getUserBlogs , updateBlog , deleteBlog } from '../controllers/blog.controller.js'
 import { authenticate } from '../controllers/user.controller.js'
 
 const router = express.Router();
 
 router.post("/create" , authenticate , createBlog);
+router.put("/update/:id" , authenticate , updateBlog);
+router.get("/all" , getAllBlogs);
+router.get("/userBlog" , authenticate , getUserBlogs);
+router.post("/blogByID/:id" , authenticate , getBlogById);
+router.delete("/delete/:id" , authenticate , deleteBlog);
+
 
 export default router;
